d23dd827d07069ae2990480f83ad54efe4f601e7
Abhijeet Jha <jhaabhijeet864@gmail.com>
2026-09-13 14:44:37 +0530

    feat: add fumadocs source loader and downgrade fumadocs dependencies

--- lib/source.ts
+++ lib/source.ts
@@ -1,11 +1,10 @@
 import { loader } from 'fumadocs-core/source';
-import { defineDocs } from 'fumadocs-mdx/macro';
-
-const docs = defineDocs({
-  dir: 'content/docs',
-});
+import { docs, meta } from '../.source';
 
 export const source = loader({
   baseUrl: '/docs',
-  source: docs.toFumadocsSource(),
+  source: {
+    files: docs.files,
+    metaFiles: meta.files,
+  },
 });
